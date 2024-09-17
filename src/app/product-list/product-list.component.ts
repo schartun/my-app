56322818a6a18848b892861fdb994ed1cd3b07fa
schartun <schartun@gmail.com>
2024-09-17 17:12:33 +0200

    check Displaying data conditionally

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-
+  products: Product[] = [];
 }
